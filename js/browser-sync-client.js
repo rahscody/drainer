ffc74c66a1d81fc8a959a59e8a3567dd0560c36f
Reklamner <109833161+Reklamner@users.noreply.github.com>
2022-12-20 16:37:48 +0400

    Add files via upload

---
+++ js/browser-sync-client.js
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Error>
+  <Code>NoSuchKey</Code>
+  <Message>The specified key does not exist.</Message>
+  <RequestId>63920175FDBA0C31326D51A5</RequestId>
+  <HostId>top100htmls.oss-cn-hongkong.aliyuncs.com</HostId>
+  <Key>v51/browser-sync-client.js</Key>
+</Error>
